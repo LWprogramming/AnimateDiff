4b1efa24b761c050eb79baa9567f8d83e10ad81d
Leon Wu <lwgithub@gmail.com>
2023-10-01 17:34:35 -0700

    add slerp by hand

--- animatediff/pipelines/pipeline_animation.py
+++ animatediff/pipelines/pipeline_animation.py
@@ -28,7 +28,7 @@ from diffusers.utils import deprecate, logging, BaseOutput
 from einops import rearrange
 
 from ..models.unet import UNet3DConditionModel
-from ..utils.util import slerp
+
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
@@ -73,6 +73,23 @@ def get_context_scheduler(name: str) -> Callable:
             return uniform
         case _:
             raise ValueError(f"Unknown context_overlap policy {name}")
+
+
+# interpolation methods
+def linear(v1, v2, t):
+    return (1.0 - t) * v1 + t * v2
+
+def slerp(
+    v0: torch.Tensor, v1: torch.Tensor, t: float, DOT_THRESHOLD: float = 0.9995
+) -> torch.Tensor:
+    u0 = v0 / v0.norm()
+    u1 = v1 / v1.norm()
+    dot = (u0 * u1).sum()
+    if dot.abs() > DOT_THRESHOLD:
+        #logger.info(f'warning: v0 and v1 close to parallel, using linear interpolation instead.')
+        return (1.0 - t) * v0 + t * v1
+    omega = dot.acos()
+    return (((1.0 - t) * omega).sin() * v0 + (t * omega).sin() * v1) / omega.sin()
 
 #### End of animatediff copied code.
 
